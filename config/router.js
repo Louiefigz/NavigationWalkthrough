8103bb7769b253ee2d6597022f59af4470d03616
iamromanh <romanh99@gmail.com>
2017-08-21 22:11:02 -0400

    Start environment CRUD

--- config/router.js
+++ config/router.js
@@ -3,22 +3,9 @@ import { TabNavigator,  StackNavigator } from 'react-navigation';
 
 import Feed from '../screens/Feed';
 import Me from '../screens/Me';
-import ChildEli from '../screens/Eli';
-
 
 import Settings from '../screens/Settings';
 import UserDetail from '../screens/UserDetail';
-
-//Stack is for more than one screen
-// export const StackOfEli = StackNavigator({
-//   ChildEli: {
-//     screen: ChildEli,
-//     navigationOptions: {
-//       title: 'Eli'
-//     }
-//   }
-// })
-
 
 export const MeStack = StackNavigator({
   Me: {
@@ -32,17 +19,9 @@ export const MeStack = StackNavigator({
     navigationOptions:{
       title: 'SettingsDuh'
     }
-  },
-  Eli: {
-    screen: ChildEli ,
-    navigationOptions: {
-      tabBarLabel: 'Eli',
-    },
   }
 
 });
-
-
 
   export const FeedStack = StackNavigator({
     Feed: {
@@ -59,8 +38,6 @@ export const MeStack = StackNavigator({
     },
   });
 
-
-
   export const Tabs = TabNavigator({
     Feed: {
       screen: FeedStack,
@@ -73,11 +50,5 @@ export const MeStack = StackNavigator({
       navigationOptions: {
         tabBarLabel: 'My Profile',
       },
-    },
-    // MySon: {
-    //   screen: StackOfEli,
-    //   navigationOptions: {
-    //     tabBarLabel: 'MySon'
-    //   },
-    // },
+    }
   });
